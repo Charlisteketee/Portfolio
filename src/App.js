756222f18eeac1ce3c58050b08508558f476c774
Charlisteketee <charlisteketee@gmail.com>
2024-07-15 12:44:18 -0700

    Ensure / goes to Home.jsx

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
